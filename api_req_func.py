e5cca573725585267b6565f9ede6a751504be97e
Leonid <leonid20.0831@gmail.com>
2023-11-23 19:04:19 +0000

    Some update

--- api_req_func.py
+++ api_req_func.py
@@ -19,7 +19,7 @@ def setup_env():
     stability_api = client.StabilityInference(
         key=os.environ["STABILITY_KEY"],  # API Key reference.
         verbose=True,  # Print debug messages.
-        engine="stable-inpainting-v1-0",  # Set the engine to use for generation.
+        engine="stable-diffusion-xl-1024-v1-0",  # Set the engine to use for generation.
         # Available engines: stable-diffusion-v1 stable-diffusion-v1-5 stable-diffusion-512-v2-0 stable-diffusion-768-v2-0
         # stable-diffusion-512-v2-1 stable-diffusion-768-v2-1 stable-diffusion-xl-beta-v2-2-2 stable-inpainting-v1-0 stable-inpainting-512-v2-0
     )
